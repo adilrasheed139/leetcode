41370a40b5e9ab541fbebc69176f075d30eb1cd8
adilrasheed139 <adilrasheed139@gmail.com>
2024-11-11 20:57:05 +0500

    first problem

---
+++ Delete Characters to Make Fancy String/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def makeFancyString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = []
+
+        for char in s:
+            if len(result) >=2 and result[-1] == char and result[-2] == char:
+                continue
+            result.append(char)
+        return "".join(result)
